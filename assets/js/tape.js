f89128c44123ea54d3eb769a90724842ccdd30f9
mayognaise <tobimayo@gmail.com>
2014-06-30 17:06:28 -0700

    updated tape class

--- assets/js/tape.js
+++ assets/js/tape.js
@@ -1,24 +1,16 @@
 var Tape;
 
 Tape = (function() {
-  function Tape(selector, y, vect, incline) {
+  function Tape(selector, vect, incline) {
     this.el = d3.select(selector);
-    this.endY = y;
     this.vect = vect;
     this.incline = incline;
     this.init();
   }
 
-  Tape.prototype.update = function(height, y, ratio){
-    var per = this.per(height, y, ratio);
+  Tape.prototype.update = function(per){
     var points = this.points(per);
     this.el.attr('points', points);
-  };
-
-  Tape.prototype.per = function(height, y, ratio){
-    var p = (this.endY - (y / ratio)) / (height / ratio);
-    p = Math.max(0, Math.min(1, p));
-    return Math.min(1, (1 - p) * 2);
   };
 
   Tape.prototype.init = function(){
@@ -53,25 +45,35 @@ Tapes = (function() {
   }
 
   Tapes.prototype.update = function(width, height, y, ratio){
+    var _initY = 3250,
+        _endY = 3700,
+        _y = y / ratio,
+        _h = height / ratio,
+        _bottom = (_y + _h - _initY) / (_endY - _initY),
+        _len = this.tapes.length,
+        _p = _bottom / _len;
     // var cy = this.center(height, y, ratio);
-    //     per = this.per(1000, height, y, ratio);
-    // console.log(cy, per);
     for(var i = 0; i < this.tapes.length; i ++){
-      this.tapes[i].update(height, y, ratio);
+      var a = (i + 1) / _len,
+          b = (_bottom - (i / _len)) * _len,
+          per = Math.max(0, Math.min(1, b));
+
+      this.tapes[i].update(per);
     }
   };
 
   Tapes.prototype.init = function(){
     this.tapes = [];
-    this.tapes.push(new Tape('#tape1', 3000, 1, -1)); // 300,3100 0,3250 0,3150 300,3000
-    this.tapes.push(new Tape('#tape2', 3400, 1, 1)); // 300,3100 900,3400 900,3300 300,3000
-    this.tapes.push(new Tape('#tape3', 3500, -1, 1)); // 900,3400 600,3550 600,3450 900,3300
-    this.tapes.push(new Tape('#tape4', 3500, 1, 1)); // 900,3600 600,3450 600,3550 900,3700
+    this.tapes.push(new Tape('#tape1', 1, -1)); // 300,3100 0,3250 0,3150 300,3000
+    this.tapes.push(new Tape('#tape2', 1, 1)); // 300,3100 900,3400 900,3300 300,3000
+    this.tapes.push(new Tape('#tape3', -1, 1)); // 900,3400 600,3550 600,3450 900,3300
+    this.tapes.push(new Tape('#tape4', 1, 1)); // 900,3600 600,3450 600,3550 900,3700
   };
 
-  // Tapes.prototype.per = function(targetY, height, y, ratio){
-  //   return (targetY - (y / ratio)) / (height / ratio);
-  // };
+  Tapes.prototype.per = function(targetY, height, y, ratio){
+    return (targetY - (y / ratio)) / (height / ratio);
+  };
+
   // Tapes.prototype.center = function(height, y, ratio){
   //   return Math.floor(((height / 2) + y) / ratio);
   // };
